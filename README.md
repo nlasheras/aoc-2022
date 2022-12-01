235491c94dfc30729db54ddfb23299e1b4af60c9
Nacho Lasheras <fanatiko@gmail.com>
2022-12-01 12:28:44 +0200

    Cleanup and write work log

--- README.md
+++ README.md
@@ -4,3 +4,6 @@ Advent of Code 2022
 ## Log
 
 ### Day 1
+
+First day is usually easy and intented for setting up stuff. I used Belen's [aoc-2021](https://github.com/belen-albeza/aoc-2021) as a template for the project. I'm a bit *rusty* in Rust and no expert, so I just made a simple solution to get the stars in. When I get more experience maybe I'll find a better way for doing this.
+
--- src/day01.rs
+++ src/day01.rs
@@ -3,21 +3,21 @@ use aoc_runner_derive::aoc_generator;
 
 #[aoc_generator(day1)]
 pub fn parse_input(input: &str) -> Vec<u64> {
-    let mut ret : Vec<u64> = input.lines().map(|x| if x.is_empty() { 0 } else { x.parse().unwrap() }).collect();
-    ret.push(0);
+    let mut ret: Vec<u64> = input
+        .lines()
+        .map(|x| if x.is_empty() { 0 } else { x.parse().unwrap() })
+        .collect();
+    ret.push(0); // make sure there is a elf "termination" to simplify the implementation
     ret
 }
 
 pub fn count_calories(entries: &[u64]) -> Vec<u64> {
     let mut sums = Vec::new();
-    let mut sum = 0;
-    for entry in entries {
-        if *entry == 0 {
-            sums.push(sum);
-            sum = 0;
-        }
-        else {
-            sum += entry;
+    let mut elf_start = 0;
+    for i in 1..entries.len() {
+        if entries[i] == 0 {
+            sums.push(entries[elf_start..i].iter().sum());
+            elf_start = i + 1;
         }
     }
     sums
@@ -27,14 +27,14 @@ pub fn count_calories(entries: &[u64]) -> Vec<u64> {
 pub fn solve_part1(entries: &[u64]) -> u64 {
     let mut calories = count_calories(entries);
     calories.sort();
-    *calories.last().expect("Entries are empty!")
+    *calories.last().expect("There is at least one elf")
 }
 
 #[aoc(day1, part2)]
 pub fn solve_part2(entries: &[u64]) -> u64 {
     let mut calories = count_calories(entries);
-    calories.sort();
-    calories[calories.len()-3..calories.len()].iter().sum()
+    calories.sort_by_key(|&n| std::cmp::Reverse(n)); // sort descent
+    calories[0..3].iter().sum()
 }
 
 #[cfg(test)]
@@ -43,14 +43,17 @@ mod tests {
 
     #[test]
     fn test_day1_part1() {
-        let input: Vec<u64> = vec![1000, 2000, 3000, 0, 4000, 0, 5000, 6000, 0, 7000, 8000, 9000, 0, 10000, 0];
+        let input: Vec<u64> = vec![
+            1000, 2000, 3000, 0, 4000, 0, 5000, 6000, 0, 7000, 8000, 9000, 0, 10000, 0,
+        ];
         assert_eq!(solve_part1(&input), 24000);
     }
 
     #[test]
     fn test_day1_part2() {
-        let input: Vec<u64> = vec![1000, 2000, 3000, 0, 4000, 0, 5000, 6000, 0, 7000, 8000, 9000, 0, 10000, 0];
+        let input: Vec<u64> = vec![
+            1000, 2000, 3000, 0, 4000, 0, 5000, 6000, 0, 7000, 8000, 9000, 0, 10000, 0,
+        ];
         assert_eq!(solve_part2(&input), 45000);
     }
-
 }
