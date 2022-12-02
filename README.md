804ce67f4851546822f1ffda06d1b7a537217be1
Nacho Lasheras <fanatiko@gmail.com>
2022-12-02 22:30:36 +0200

    Cleanup Day 2

--- README.md
+++ README.md
@@ -7,3 +7,6 @@ Advent of Code 2022
 
 First day is usually easy and intented for setting up stuff. I used Belen's [aoc-2021](https://github.com/belen-albeza/aoc-2021) as a template for the project. I'm a bit *rusty* in Rust and no expert, so I just made a simple solution to get the stars in. When I get more experience maybe I'll find a better way for doing this.
 
+### Day 2
+
+Second day I started with a quick and dirty solution without any kind of Rust idiom. In the afternoon I had some free time that I used to clean the solution a bit and I used the `match` expressions quite extensively. 
--- src/day02.rs
+++ src/day02.rs
@@ -3,85 +3,104 @@ use aoc_runner_derive::aoc_generator;
 
 #[aoc_generator(day2)]
 pub fn parse_input(input: &str) -> Vec<(char, char)> {
-    input.lines().map(|s| {
-        let mut chars = s.chars();
-        (chars.nth(0).unwrap(), chars.nth(1).unwrap()) // NOTE: nth consumes
-    }).collect()
+    input
+        .lines()
+        .map(|s| {
+            let mut chars = s.chars();
+            (chars.nth(0).unwrap(), chars.nth(1).unwrap()) // NOTE: nth consumes
+        })
+        .collect()
 }
 
 #[derive(PartialEq, Clone, Copy)]
 pub enum Shape {
     Rock = 1,
     Paper = 2,
-    Scissors = 3
+    Scissors = 3,
 }
 
-pub fn beats(this: Shape, other: Shape) -> bool {
-    (this == Shape::Rock && other == Shape::Scissors) ||
-    (this == Shape::Paper && other == Shape::Rock) ||
-    (this == Shape::Scissors && other == Shape::Paper)
-}
-
-pub fn get_score(you: Shape, oponent: Shape) -> u64
-{
-    if beats(you, oponent) { 
-        return 6
+impl Shape {
+    fn from_i64(n: i64) -> Shape {
+        return match n {
+            1 => Shape::Rock,
+            2 => Shape::Paper,
+            3 => Shape::Scissors,
+            _ => panic!("Unknown shape!"),
+        };
     }
-    if beats(oponent, you) {
-        return 0
+
+    pub fn beats_shape(&self) -> Shape {
+        let as_num = *self as i64;
+        let previous_shape = (as_num - 2).rem_euclid(3) + 1;
+        Shape::from_i64(previous_shape)
     }
-    return 3
+
+    pub fn is_beaten_by_shape(&self) -> Shape {
+        let as_num = *self as i64;
+        let next_shape = as_num.rem_euclid(3) + 1;
+        Shape::from_i64(next_shape)
+    }
+
+    pub fn value(&self) -> u64 {
+        *self as u64
+    }
 }
 
-pub fn get_shape(c: char) -> Shape
-{
-    if c == 'A' || c == 'X' { return Shape::Rock } 
-    if c == 'B' || c == 'Y' { return Shape::Paper } 
-    return Shape::Scissors
+pub fn get_score(player: Shape, opponent: Shape) -> u64 {
+    if player.beats_shape() == opponent {
+        return 6;
+    }
+    if opponent.beats_shape() == player {
+        return 0;
+    }
+    return 3;
+}
+
+pub fn decode_shape(c: char) -> Shape {
+    return match c {
+        'A' | 'X' => Shape::Rock,
+        'B' | 'Y' => Shape::Paper,
+        'C' | 'Z' => Shape::Scissors,
+        _ => panic!("Unsupported char in strategy"),
+    };
 }
 
 #[aoc(day2, part1)]
 pub fn solve_part1(strategy: &Vec<(char, char)>) -> u64 {
     let mut score = 0;
-    for (coponent, cyou) in strategy {
-        let you = get_shape(*cyou);
-        let oponent = get_shape(*coponent);
-        score += get_score(you, oponent) + you as u64;
+    for (opponent_play, suggested_play) in strategy {
+        let player_shape = decode_shape(*suggested_play);
+        let opponent_shape = decode_shape(*opponent_play);
+        score += get_score(player_shape, opponent_shape) + player_shape.value();
     }
     score
 }
 
-pub fn get_right_shape(score: u64, oponent: Shape) -> Shape {
-    if score == 3 { return oponent }
-    if score == 6 {  // win
-        return match oponent {
-            Shape::Rock => Shape::Paper,
-            Shape::Scissors => Shape::Rock,
-            Shape::Paper => Shape::Scissors
-        }
-    }
-    else {  // lose
-        return match oponent {
-            Shape::Paper => Shape::Rock,
-            Shape::Rock => Shape::Scissors,
-            Shape::Scissors => Shape::Paper
-        }
-    }
+pub fn get_right_shape(score: u64, opponent: Shape) -> Shape {
+    return match score {
+        3 => opponent,                      // draw
+        6 => opponent.is_beaten_by_shape(), // win
+        0 => opponent.beats_shape(),        // lose
+        _ => panic!("Unsupported outcome!"),
+    };
 }
 
-pub fn letter_into_outcome(c: char) -> u64 {
-    if c == 'Y' { return 3 }
-    if c == 'Z' { return 6 }
-    return 0
+pub fn decode_outcome(c: char) -> u64 {
+    return match c {
+        'X' => 0,
+        'Y' => 3,
+        'Z' => 6,
+        _ => panic!("Unsupported outcome in strategy!"),
+    };
 }
 
 #[aoc(day2, part2)]
 pub fn solve_part2(strategy: &Vec<(char, char)>) -> u64 {
     let mut score = 0;
-    for (coponent, coutcome) in strategy {
-        let oponent = get_shape(*coponent);
-        let outcome = letter_into_outcome(*coutcome);
-        score += outcome + get_right_shape(outcome, oponent) as u64;
+    for (opponent_play, outcome) in strategy {
+        let oponent_shape = decode_shape(*opponent_play);
+        let outcome_score = decode_outcome(*outcome);
+        score += outcome_score + get_right_shape(outcome_score, oponent_shape).value();
     }
     score
 }
@@ -92,18 +111,13 @@ mod tests {
 
     #[test]
     fn test_day1_part1() {
-        let input: Vec<(char, char)> = vec![
-            ('A', 'Y'), ('B', 'X'), ('C', 'Z')
-        ];
+        let input: Vec<(char, char)> = vec![('A', 'Y'), ('B', 'X'), ('C', 'Z')];
         assert_eq!(solve_part1(&input), 15);
     }
 
     #[test]
     fn test_day1_part2() {
-        let input: Vec<(char, char)> = vec![
-            ('A', 'Y'), ('B', 'X'), ('C', 'Z')
-        ];
+        let input: Vec<(char, char)> = vec![('A', 'Y'), ('B', 'X'), ('C', 'Z')];
         assert_eq!(solve_part2(&input), 12);
     }
-    
 }
