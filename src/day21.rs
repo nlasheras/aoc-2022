107af96747b572939859977c01b70dffeaae1d4b
Nacho Lasheras <fanatiko@gmail.com>
2022-12-21 08:43:24 +0200

    Solution for part 1 of day 21

---
+++ src/day21.rs
@@ -0,0 +1,87 @@
+use aoc_runner_derive::aoc;
+use aoc_runner_derive::aoc_generator;
+use std::collections::HashMap;
+
+#[derive(Debug)]
+pub enum Monkey 
+{
+    Number(i64),
+    Add(String, String),
+    Sub(String, String),
+    Mul(String, String),
+    Div(String, String)
+}
+
+impl Monkey
+{
+    fn from(input: &str) -> Monkey {
+        if let Ok(number) = input.parse::<i64>() {
+            return Monkey::Number(number)
+        }
+        let parts = input.split(" ").collect::<Vec<&str>>();
+        match parts[1] {
+            "+" => Monkey::Add(parts[0].to_string(), parts[2].to_string()),       
+            "-" => Monkey::Sub(parts[0].to_string(), parts[2].to_string()),      
+            "*" => Monkey::Mul(parts[0].to_string(), parts[2].to_string()),      
+            "/" => Monkey::Div(parts[0].to_string(), parts[2].to_string()),       
+            _ => panic!("Wrong op {}", parts[1])
+        }
+    }
+}
+
+type Monkeys = HashMap<String, Monkey>;
+
+#[aoc_generator(day21)]
+pub fn parse_input(input: &str) -> Monkeys {
+    let mut map = HashMap::new();
+    input.lines().for_each(|s| { 
+        let parts = s.split(": ").collect::<Vec<&str>>();
+        map.insert(parts[0].to_string(), Monkey::from(parts[1]));
+    });
+    map
+}
+
+
+fn get_monkey(map: &Monkeys, key: &str) -> i64 {
+    let monkey = &map[key];
+    match monkey {
+        Monkey::Number(n) => *n,
+        Monkey::Add(k1, k2) => get_monkey(map, &k1) + get_monkey(map, &k2),
+        Monkey::Sub(k1, k2) => get_monkey(map, &k1) - get_monkey(map, &k2),
+        Monkey::Mul(k1, k2) => get_monkey(map, &k1) * get_monkey(map, &k2),
+        Monkey::Div(k1, k2) => get_monkey(map, &k1) / get_monkey(map, &k2),
+    }
+}
+
+#[aoc(day21, part1)]
+pub fn get_root(input: &Monkeys) -> i64 {
+    get_monkey(input, "root")
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const DAY21_EXAMPLE: &str = "root: pppw + sjmn
+dbpl: 5
+cczh: sllz + lgvd
+zczc: 2
+ptdq: humn - dvpt
+dvpt: 3
+lfqf: 4
+humn: 5
+ljgn: 2
+sjmn: drzm * dbpl
+sllz: 4
+pppw: cczh / lfqf
+lgvd: ljgn * ptdq
+drzm: hmdt - zczc
+hmdt: 32";
+
+    #[test]
+    fn test_day21_part1() {
+        let input = parse_input(DAY21_EXAMPLE);
+        assert_eq!(get_root(&input), 152);
+    }
+
+}
